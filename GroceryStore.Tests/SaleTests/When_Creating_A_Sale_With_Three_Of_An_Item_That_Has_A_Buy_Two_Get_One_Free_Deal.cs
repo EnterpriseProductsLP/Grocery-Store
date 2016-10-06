c12e83fbab94a47dff58734a2ba1a420d737f9e8
Eric Burcham <eburcham@eprod.com>
2016-10-06 16:52:48 -0500

    Updated instantiation to use primitives in an uneccessarily complex test fixture.

--- GroceryStore.Tests/SaleTests/When_Creating_A_Sale_With_Three_Of_An_Item_That_Has_A_Buy_Two_Get_One_Free_Deal.cs
+++ GroceryStore.Tests/SaleTests/When_Creating_A_Sale_With_Three_Of_An_Item_That_Has_A_Buy_Two_Get_One_Free_Deal.cs
@@ -7,21 +7,17 @@ namespace GroceryStore.Tests.SaleTests
     [TestFixture]
     class When_Creating_A_Sale_With_Three_Of_An_Item_That_Has_A_Buy_Two_Get_One_Free_Deal
     {
-        private Item _item;
-
         private Sale _sale;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
         {
-            _item = ItemBuilder.BuildItem("1245");
-
-            DealProvider.AddDeal(_item.Sku, new BuySomeGetOneFreeDeal(2));
+            DealProvider.AddDeal("1245", new BuySomeGetOneFreeDeal(2));
 
             _sale = new Sale();
-            _sale.AddItem(_item.Sku);
-            _sale.AddItem(_item.Sku);
-            _sale.AddItem(_item.Sku);
+            _sale.AddItem("1245");
+            _sale.AddItem("1245");
+            _sale.AddItem("1245");
         }
 
         [OneTimeTearDown]
@@ -33,7 +29,7 @@ namespace GroceryStore.Tests.SaleTests
         [Test]
         public void Total_Should_Be_The_Item_Price_Times_Two()
         {
-            _sale.Total.Should().Be(2 * _item.Price);
+            _sale.Total.Should().Be(2 * 1.25M);
         }
     }
 }
