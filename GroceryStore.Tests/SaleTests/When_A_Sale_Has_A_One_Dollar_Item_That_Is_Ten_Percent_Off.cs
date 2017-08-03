dd7028dc9e8a7c04c49f6227d68affd77af4201b
Eric Burcham <jameseburcham@gmail.com>
2017-08-03 11:38:35 -0500

    Finished unit tests for ten percent off deal.

---
+++ GroceryStore.Tests/SaleTests/When_A_Sale_Has_A_One_Dollar_Item_That_Is_Ten_Percent_Off.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using GroceryStore.Deals;
+using GroceryStore.Domain;
+using NUnit.Framework;
+
+namespace GroceryStore.Tests.SaleTests
+{
+    [TestFixture]
+    public class When_A_Sale_Has_An_Item_That_Is_Ten_Percent_Off
+    {
+        private DealConfigurator _dealConfigurator;
+        private Sale _sale;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Arrange ----------------------------------------------------------------
+            // 1. Item configured with a 10% discount.
+            _dealConfigurator = new DealConfigurator();
+            _dealConfigurator.AddDeal("1245", new TenPercentDiscountDeal());
+
+            // 2. A sale
+            _sale = new Sale(_dealConfigurator);
+            // ------------------------------------------------------------------------
+            
+            // Act --------------------------------------------------------------------
+            // 1. Add an item with the ten percent discount to the sale.
+            _sale.AddItem("1245");
+        }
+
+        [Test]
+        public void The_Sale_Total_Should_Be_90_Percent_Of_The_Item_Price()
+        {
+            var bananas = ItemBuilder.BuildItem("1245");
+            var priceOfBananas = bananas.Price;
+            var expectedSaleTotal = Math.Round(priceOfBananas * 0.9M, 2);
+            _sale.Total.Should().Be(expectedSaleTotal);
+        }
+    }
+}
---
+++ GroceryStore/Deals/TenPercentDiscountDeal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GroceryStore.Interfaces;
+
+namespace GroceryStore.Deals
+{
+    public class TenPercentDiscountDeal : IDeal
+    {
+        public decimal GetDiscount(uint quantity, decimal price)
+        {
+            var rawDiscount = (quantity * price) * 0.1M;
+            var decimalDiscount = Math.Round(rawDiscount, 2, MidpointRounding.AwayFromZero);
+            return decimalDiscount;
+        }
+    }
+}
