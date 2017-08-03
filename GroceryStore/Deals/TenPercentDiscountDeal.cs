16b5823dee199c3edb1bbc92813a367915bd335b
Eric Burcham <jameseburcham@gmail.com>
2017-08-03 11:43:20 -0500

    Refactored ten percent off deal.

--- GroceryStore/Deals/TenPercentDiscountDeal.cs
+++ GroceryStore/Deals/TenPercentDiscountDeal.cs
@@ -3,13 +3,27 @@ using GroceryStore.Interfaces;
 
 namespace GroceryStore.Deals
 {
-    public class TenPercentDiscountDeal : IDeal
+    public abstract class PercentageOffDeal : IDeal
     {
+        private readonly decimal _discountPercent;
+
+        protected PercentageOffDeal(decimal discountPercent)
+        {
+            _discountPercent = discountPercent;
+        }
+
         public decimal GetDiscount(uint quantity, decimal price)
         {
-            var rawDiscount = (quantity * price) * 0.1M;
-            var decimalDiscount = Math.Round(rawDiscount, 2, MidpointRounding.AwayFromZero);
-            return decimalDiscount;
+            var rawDiscount = (quantity * price) * _discountPercent;
+            var roundedDiscount = Math.Round(rawDiscount, 2, MidpointRounding.AwayFromZero);
+            return roundedDiscount;
+        }
+    }
+    
+    public class TenPercentDiscountDeal : PercentageOffDeal
+    {
+        public TenPercentDiscountDeal() : base(0.1M)
+        {
         }
     }
 }
